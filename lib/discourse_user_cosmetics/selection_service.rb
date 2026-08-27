c29863366ff2d39cef28d2fe296f5225e6f3b165
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-28 00:04:04 +0300

    FIX: centralize cosmetic selection integrity

---
+++ lib/discourse_user_cosmetics/selection_service.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+module ::DiscourseUserCosmetics
+  class SelectionService
+    def self.select!(user:, kind:, item_id:)
+      kind = kind.to_s
+      raise Discourse::InvalidParameters.new(:kind) unless DiscourseUserCosmetics::Item::KINDS.include?(kind)
+
+      item = nil
+      if item_id.present?
+        item = DiscourseUserCosmetics::Item.find_by(id: item_id, kind: kind, enabled: true)
+        raise Discourse::NotFound unless item
+        raise Discourse::InvalidAccess unless item.usable_by?(user)
+      end
+
+      selection = DiscourseUserCosmetics::UserSelection.find_or_initialize_by(user_id: user.id)
+      selection.public_send("#{DiscourseUserCosmetics::UserSelection.field_for(kind)}=", item&.id)
+      selection.save!
+
+      DiscourseUserCosmetics::Presenter.bump_version!
+      selection
+    end
+
+    def self.clear_item!(item)
+      field = DiscourseUserCosmetics::UserSelection.field_for(item.kind)
+      changed =
+        DiscourseUserCosmetics::UserSelection.where(field => item.id).update_all(
+          field => nil,
+          updated_at: Time.zone.now,
+        )
+
+      DiscourseUserCosmetics::Presenter.bump_version! if changed.positive?
+      changed
+    end
+
+    def self.clear_item_for_user_if_unusable!(item:, user:)
+      return 0 if item.enabled? && item.usable_by?(user)
+
+      field = DiscourseUserCosmetics::UserSelection.field_for(item.kind)
+      changed =
+        DiscourseUserCosmetics::UserSelection.where(user_id: user.id, field => item.id).update_all(
+          field => nil,
+          updated_at: Time.zone.now,
+        )
+
+      DiscourseUserCosmetics::Presenter.bump_version! if changed.positive?
+      changed
+    end
+
+    def self.clear_invalid_for_item!(item)
+      field = DiscourseUserCosmetics::UserSelection.field_for(item.kind)
+      changed = 0
+
+      DiscourseUserCosmetics::UserSelection.where(field => item.id).includes(:user).find_each do |selection|
+        user = selection.user
+        next if user && item.enabled? && item.usable_by?(user)
+
+        selection.update_column(field, nil)
+        selection.touch
+        changed += 1
+      end
+
+      DiscourseUserCosmetics::Presenter.bump_version! if changed.positive?
+      changed
+    end
+  end
+end
