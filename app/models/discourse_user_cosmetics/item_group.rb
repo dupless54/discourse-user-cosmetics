c8d0d2e767cbb95d201261286675ada352ca0b9f
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-28 00:20:45 +0300

    FIX: keep group access cleanup out of association callbacks

--- app/models/discourse_user_cosmetics/item_group.rb
+++ app/models/discourse_user_cosmetics/item_group.rb
@@ -6,14 +6,5 @@ module ::DiscourseUserCosmetics
 
     belongs_to :item, class_name: "DiscourseUserCosmetics::Item"
     belongs_to :group, class_name: "::Group"
-
-    after_create_commit :clear_newly_invalid_selections
-    after_update_commit :clear_newly_invalid_selections, if: :saved_change_to_group_id?
-
-    private
-
-    def clear_newly_invalid_selections
-      DiscourseUserCosmetics::SelectionService.clear_invalid_for_item!(item)
-    end
   end
 end
