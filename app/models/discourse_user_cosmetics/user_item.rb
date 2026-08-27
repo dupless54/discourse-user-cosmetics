613b2e7eb4462f8a17ee87a8a6aea7a5265ad1e5
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-28 00:05:18 +0300

    FIX: clear unusable selections after ownership revoke

--- app/models/discourse_user_cosmetics/user_item.rb
+++ app/models/discourse_user_cosmetics/user_item.rb
@@ -7,5 +7,13 @@ module ::DiscourseUserCosmetics
     belongs_to :user, class_name: "::User"
     belongs_to :item, class_name: "DiscourseUserCosmetics::Item"
     belongs_to :granted_by, class_name: "::User", optional: true
+
+    after_destroy :clear_unusable_active_selection
+
+    private
+
+    def clear_unusable_active_selection
+      DiscourseUserCosmetics::SelectionService.clear_item_for_user_if_unusable!(item: item, user: user)
+    end
   end
 end
