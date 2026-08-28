31d4c9cdfb8836bf2ec2d4685bc9325f61bb2a8a
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-28 20:37:11 +0300

    TEST: cover extended cosmetic entitlement rules

---
+++ spec/lib/entitlement_resolver_extension_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+RSpec.describe DiscourseUserCosmetics::EntitlementResolver do
+  fab!(:user)
+
+  it "honors an extended item access rule that restricts base access" do
+    item = DiscourseUserCosmetics::Item.create!(kind: "avatar_frame", name: "Store locked frame")
+    item.define_singleton_method(:usable_by?) { |_candidate| false }
+
+    usable = described_class.usable_item_ids(user: user, items: [item])
+
+    expect(usable).not_to have_key(item.id)
+  end
+
+  it "honors an extended item access rule that grants access" do
+    restricted_group = Fabricate(:group)
+    item = DiscourseUserCosmetics::Item.create!(kind: "avatar_frame", name: "Extended frame")
+    item.item_groups.create!(group: restricted_group)
+    item.define_singleton_method(:usable_by?) { |candidate| candidate.id == user.id }
+
+    usable = described_class.usable_item_ids(user: user, items: [item])
+
+    expect(usable).to include(item.id => true)
+  end
+end
