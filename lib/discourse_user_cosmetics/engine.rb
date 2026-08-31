dff9df11d1fbaa823edeeb950ce6130c644dc020
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-31 04:27:24 +0300

    REFINE: keep non-Zeitwerk lib extensions explicit

--- lib/discourse_user_cosmetics/engine.rb
+++ lib/discourse_user_cosmetics/engine.rb
@@ -4,6 +4,5 @@ module ::DiscourseUserCosmetics
   class Engine < ::Rails::Engine
     engine_name PLUGIN_NAME
     isolate_namespace DiscourseUserCosmetics
-    config.autoload_paths << File.join(config.root, "lib")
   end
 end
