f5651a343b2d111ab8c8e30173fc1dfc1b241a74
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-31 04:34:11 +0300

    REFACTOR: make showcase integration Zeitwerk-compatible

--- lib/discourse_user_cosmetics/showcase_integration.rb
+++ lib/discourse_user_cosmetics/showcase_integration.rb
@@ -1,23 +1,21 @@
 # frozen_string_literal: true
 
 module ::DiscourseUserCosmetics
-  class Integration
-    class << self
-      def showcase_supported?
-        true
-      end
+  module ShowcaseIntegration
+    def showcase_supported?
+      true
+    end
 
-      def showcase_for(user:)
-        raise ArgumentError, "user is required" unless user
+    def showcase_for(user:)
+      raise ArgumentError, "user is required" unless user
 
-        DiscourseUserCosmetics::ShowcaseService.serialize_for(user: user)
-      end
+      DiscourseUserCosmetics::ShowcaseService.serialize_for(user: user)
+    end
 
-      def update_showcase!(user:, item_ids:)
-        raise ArgumentError, "user is required" unless user
+    def update_showcase!(user:, item_ids:)
+      raise ArgumentError, "user is required" unless user
 
-        DiscourseUserCosmetics::ShowcaseService.replace!(user: user, item_ids: item_ids)
-      end
+      DiscourseUserCosmetics::ShowcaseService.replace!(user: user, item_ids: item_ids)
     end
   end
 end
